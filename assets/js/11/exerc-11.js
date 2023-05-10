032663ff51037c30a2872608dd1e2fd1fe181358
Izabel Nascimento <izabel.nascimento@base.digital>
2023-05-10 16:53:41 -0300

    Adicionando JavaScript

---
+++ assets/js/11/exerc-11.js
@@ -0,0 +1,26 @@
+/*11 - Crie 3 eventos usando o “addEventListener” do Javascript no site criado por vocês no treinamento de HTML e CSS. 
+Esses eventos devem fazer sentido dentro do site.*/
+let tatuador = document.querySelector('.estilo-sep');
+
+
+tatuador.addEventListener("mouseover", function nomeTaduador() {
+    document.querySelector('.estilo-tatuador').innerHTML = "Renata Luz";
+    document.querySelector('.estilo-tatuador.geo').innerHTML = "Kaike Soares";
+    document.querySelector('.estilo-tatuador.black').innerHTML = "Melanie Rosa";
+    document.querySelector('.estilo-tatuador.ver').innerHTML = "Astrid Rouge";
+})
+
+tatuador.addEventListener("mouseout", function nomeTaduadorSai() {
+    document.querySelector('.estilo-tatuador').innerHTML = "";
+    document.querySelector('.estilo-tatuador.geo').innerHTML = "";
+    document.querySelector('.estilo-tatuador.black').innerHTML = "";
+    document.querySelector('.estilo-tatuador.ver').innerHTML = "";
+})
+
+
+let btn = document.querySelector('.botaoS');
+
+btn.addEventListener("click", function submetido() {
+    alert("Agendamento solicitado com sucesso!");
+})
+
---
+++ assets/js/12/exerc-12.js
@@ -0,0 +1,1 @@
+/*12 - Implemente um Carrossel no site de vocês usando esta biblioteca: https://swiperjs.com/get-started*/
